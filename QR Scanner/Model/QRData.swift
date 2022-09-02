58bab91bad0ab61c44b9799682080a4820651d35
fjbMultisys <jaybergonia@Jays-MacBook-Pro.local>
2022-09-02 14:22:02 +0800

    basic feature set

---
+++ QR Scanner/Model/QRData.swift
@@ -0,0 +1,12 @@
+//
+//  QRData.swift
+//  QRScanner
+//
+//  Created by Jay Bergonia on 9/2/22.
+//
+
+import Foundation
+
+struct QRData {
+    var codeString: String?
+}
---
+++ QR Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR Scanner
+//
+//  Created by Jay Bergonia on 9/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
